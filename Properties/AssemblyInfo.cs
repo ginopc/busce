cbc49c74e12048595a1647cc1d2f9161800b5519
ginopc <ginoc@tiscali.it>
2014-06-03 17:06:49 +0200

    Release 0.0.2 (vedi README.txt)

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("RAPIDemo")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("BusCE")]
+[assembly: AssemblyDescription("Utility di ricezione files")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("RAPIDemo")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCompany("S.C.S.")]
+[assembly: AssemblyProduct("BusCE")]
+[assembly: AssemblyCopyright("Copyright © 2014 by Maurizio Aru")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.0.2.*")]
+[assembly: AssemblyFileVersion("0.0.1.0")]
